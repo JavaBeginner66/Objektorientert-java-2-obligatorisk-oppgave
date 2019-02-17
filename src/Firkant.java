bbdc0e2ec8ae46495e109465b6a2ba99c0efcd87
Jan Andreas <jan-anan@hotmail.com>
2019-02-17 22:27:15 +0100

    La til firkant-klasse

---
+++ src/Firkant.java
@@ -0,0 +1,11 @@
+import javafx.scene.control.CheckBox;
+import javafx.scene.shape.Rectangle;
+
+public class Firkant extends Rectangle implements CheckCollision{
+
+    public void collision(Ball ball)
+    {
+        
+    }
+
+}
