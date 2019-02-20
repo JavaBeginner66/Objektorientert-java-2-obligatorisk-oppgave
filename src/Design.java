7e5c526a00286eb4d85052c40577681dbbd4ad77
Jan Andreas <jan-anan@hotmail.com>
2019-02-20 15:25:00 +0100

    Design-klasse setup

--- src/Design.java
+++ src/Design.java
@@ -1,6 +1,24 @@
+import javafx.scene.shape.Shape;
+
 import java.util.ArrayList;
 
 public class Design {
 
-    public ArrayList<CollisionObjects> objekt;
+    private ArrayList<Shape> objektArray;
+    private PinballBoard board;
+
+    public Design(PinballBoard board){
+        this.board = board;
+        objectSetup();
+        board.getMainPane().getChildren().addAll(objektArray);
+    }
+
+    private void objectSetup(){
+
+    }
+
+    public ArrayList<Shape> getObjectArray(){
+        return objektArray;
+    }
+
 }
--- src/MyRectangle.java
+++ src/MyRectangle.java
@@ -4,7 +4,9 @@ public class MyRectangle extends Rectangle implements CollisionObjects {
 
     public void collision(Ball ball)
     {
-
+        // sjekk for kollisjon med ball
+            // if kollisjon, fra hvilken vinkel?
+                // Gi ball ny vinkel/fart
     }
 
 }
