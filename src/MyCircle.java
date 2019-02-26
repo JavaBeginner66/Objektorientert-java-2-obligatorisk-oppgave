e96d7c90aad8b043c7f060a28d7bb85db9a41e9f
Jan Andreas <jan-anan@hotmail.com>
2019-02-26 17:27:22 +0100

    Sirkel class

---
+++ src/MyCircle.java
@@ -0,0 +1,20 @@
+import javafx.geometry.Bounds;
+import javafx.scene.shape.Circle;
+
+public class MyCircle extends Circle implements CollisionObjects{
+
+    public MyCircle(double x, double y, double radius){
+        super(x, y, radius);
+    }
+
+    public boolean detectCollision(Ball ball)
+    {
+        Bounds player = ball.getBoundsInLocal();
+        Bounds thisRect = this.getBoundsInLocal();
+        return player.intersects(thisRect);
+    }
+
+    public void collisionEvent(Ball ball){
+
+    }
+}
