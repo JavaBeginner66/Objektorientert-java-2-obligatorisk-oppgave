dbd036b76885a067c225f4872300835deb882cd1
Jan Andreas <jan-anan@hotmail.com>
2019-02-26 17:50:47 +0100

    Objekt test

--- src/Design.java
+++ src/Design.java
@@ -7,8 +7,6 @@ public class Design {
     private ArrayList<CollisionObjects> objektArray;
     private PinballBoard board;
 
-    public MyRectangle r1;
-
     public Design(PinballBoard board) {
         this.board = board;
         objectSetup();
@@ -17,8 +15,9 @@ public class Design {
     private void objectSetup(){
         objektArray = new ArrayList<>();
 
-        MyRectangle r1 = new MyRectangle(100, 100, 200, 200);
-        objektArray.add(r1);
+        MyCircle c1 = new MyCircle(200, 500, 50);
+
+        objektArray.add(c1);
 
         for (CollisionObjects anObjektArray : objektArray)
             board.getMainPane().getChildren().addAll((Shape) anObjektArray);
