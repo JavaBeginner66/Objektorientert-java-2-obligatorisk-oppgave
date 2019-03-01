6e8a84c7fd182ce48e46e1938ac9aebcbceab924
Jan Andreas <jan-anan@hotmail.com>
2019-03-01 16:26:50 +0100

    Balls left teller ned når ball går ut av spill

--- src/GameManager.java
+++ src/GameManager.java
@@ -9,24 +9,44 @@ public class GameManager {
     public static boolean gameOver;
 
     public GameBoard board;
+    public Ball ball;
+    private Player player;
+
+    private Animation animation;
 
     public GameManager(GameBoard board){
         this.board = board;
+        player = new Player();
         gameRunning = false;
         newBall();
     }
 
     public void newBall(){
+        if(animation != null)
+            animation.stop();
 
-        Ball ball = new Ball(600, 800, 20, board);
+        if(!ballsLeft())
+            System.out.println("Game over");
+
+        ball = new Ball(570, 800, 20, board);
         board.getMainPane().getChildren().add(ball);
+        System.out.println("   Velocity x: " + ball.getVelocity().getX() + "   Velocity y: " +  ball.getVelocity().getY());
 
-        Animation animation = new Timeline(new KeyFrame(Duration.millis(30), e -> board.moveBall(ball)));
+        animation = new Timeline(new KeyFrame(Duration.millis(30), e -> board.moveBall(ball)));
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.play();
 
         ball.setOnMouseClicked(e -> gameRunning = true);
-        System.out.print("Newball");
+    }
+
+    private boolean ballsLeft(){
+
+        ScoreGui.ballsLeft.setText("Balls: " + player.getBalls());
+        player.setBalls(player.getBalls()-1);
+        System.out.print(player.getBalls());
+
+        return!(player.getBalls() < 0);
+
     }
 
 }
