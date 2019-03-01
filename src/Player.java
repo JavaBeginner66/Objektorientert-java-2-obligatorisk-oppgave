ee4e57c377d7aaf167c7aa0b1ba6f47a4c490d4f
Jan Andreas <jan-anan@hotmail.com>
2019-03-01 16:25:43 +0100

    Player klasse utvida med get/set metoder

--- src/Player.java
+++ src/Player.java
@@ -7,4 +7,20 @@ public class Player {
         this.balls = 5;
         this.score = 0;
     }
+
+    public int getBalls() {
+        return balls;
+    }
+
+    public void setBalls(int balls) {
+        this.balls = balls;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
