ddbbc106c954a70833d0624292e85c92b353c50c
Jan Andreas <jan-anan@hotmail.com>
2019-03-03 12:56:02 +0100

    MyLine doc

--- src/MyLine.java
+++ src/MyLine.java
@@ -1,6 +1,10 @@
 import javafx.geometry.Bounds;
 import javafx.scene.shape.Line;
 
+/**
+ * Klassen produserer linje-objekt av type Shape
+ */
+
 public class MyLine extends Line implements CollisionObjects{
 
     public MyLine(double startX, double startY, double endX, double endY){
@@ -8,6 +12,9 @@ public class MyLine extends Line implements CollisionObjects{
         setStrokeWidth(7);
     }
 
+    /**
+     * Metoden sjekker kollisjon med Bounds klassen
+     */
     public boolean detectCollision(Ball ball)
     {
 
@@ -16,8 +23,10 @@ public class MyLine extends Line implements CollisionObjects{
         return player.intersects(thisRect);
     }
 
+    /**
+     * Metoden vil gi ballen en ny velocity
+     */
     public void collisionEvent(Ball ball){
-        // Hardkoder ball til å sprette mot venstre
         ball.setVelocity(new BallVector(ball.getVelocity().getX()-10, 10));
     }
 }
