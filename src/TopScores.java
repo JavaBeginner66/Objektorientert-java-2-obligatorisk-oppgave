365a770dffcf16cefa58933570c2662e99cb1194
Jan Andreas <jan-anan@hotmail.com>
2019-03-03 13:24:34 +0100

    TopScores doc

--- src/TopScores.java
+++ src/TopScores.java
@@ -11,15 +11,23 @@ import javafx.scene.text.FontWeight;
 import java.io.*;
 import java.util.Collections;
 
+/**
+ * Klasse oppretter ett Gameover panel som viser
+ * en topp 10 liste over høyeste scorende spillere
+ */
+
 public class TopScores extends GridPane {
 
-    public Button playAgain;
+    public final Button playAgain;
 
     private ObservableList<Player> scoreList = FXCollections.observableArrayList();
 
+    /**
+     * Metoden plasserer og styler labels
+     */
     public TopScores(){
 
-        Label gameOver = new Label("Game over. \n Toplist: ");
+        final Label gameOver = new Label("Game over. \n Toplist: ");
         gameOver.setFont(Font.font("Monospace",
                 FontWeight.BOLD, FontPosture.REGULAR, 30));
         playAgain = new Button("Spill igjen");
@@ -36,6 +44,10 @@ public class TopScores extends GridPane {
 
     }
 
+    /**
+     * Metoden skriver et nytt spiller-objekt til score-fil
+     * for hver gang spiller når ''Game over''.
+     */
     public void writeToScoreList(Player player){
 
         FileOutputStream fileOut;
@@ -57,6 +69,10 @@ public class TopScores extends GridPane {
         writeFromScoreList();
     }
 
+    /**
+     * Metoden skriver spiller-objekt fra score-fil og
+     * legger dem inn i scoreList tabellen.
+     */
     public void writeFromScoreList(){
 
         FileInputStream fileIn;
@@ -85,19 +101,26 @@ public class TopScores extends GridPane {
         }catch(Exception e){}
     }
 
+    /**
+     * Metoden sorterer spiller-objekt lista på score, og
+     * legger den på panelet.
+     */
     private void displayTopList(){
 
         Collections.sort(scoreList);
         Collections.reverse(scoreList);
 
         for(int i = 0; i<10; i++){
-            Label scorer = new Label(i+1 + ": " + scoreList.get(i));
+            final Label scorer = new Label(i+1 + ": " + scoreList.get(i));
             scorer.setPadding(new Insets(10, 0, 10, 0));
             setStyle(scorer);
             this.add(scorer, 0, i+1);
         }
     }
 
+    /**
+     * Metoden beskriver skrifts-stil.
+     */
     private void setStyle(Label label){
         label.setFont(Font.font("Monospace",
                 FontWeight.BOLD, FontPosture.REGULAR, 18));
