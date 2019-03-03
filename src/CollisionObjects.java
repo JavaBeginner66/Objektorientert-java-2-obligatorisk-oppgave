a768b1f6d450ff614561317e031d90c4fc56aa50
Jan Andreas <jan-anan@hotmail.com>
2019-03-03 12:14:44 +0100

    CollisionObjects doc

--- src/CollisionObjects.java
+++ src/CollisionObjects.java
@@ -1,6 +1,21 @@
+/**
+ * Grensesnittet implementerer metoder for
+ * sjekk og handling av kollisjon mellom
+ * spiller-ball og shapes
+ */
+
 public interface CollisionObjects {
 
+    /**
+     * Metoden leter etter en kollisjon
+     * og returnerer true om funnet
+     */
     public boolean detectCollision(Ball ball);
+
+    /**
+     * Metoden beskriver hva som skjer med
+     * ball-feltet om en kollisjon skulle skje
+     */
     public void collisionEvent(Ball ball);
 
 }
