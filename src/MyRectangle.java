368490182398c939b2ecd9b595dcd4b701ce990c
Jan Andreas <jan-anan@hotmail.com>
2019-03-03 12:56:11 +0100

    MyRectangle doc

--- src/MyRectangle.java
+++ src/MyRectangle.java
@@ -4,12 +4,19 @@ import javafx.scene.shape.Rectangle;
 import java.awt.*;
 import java.awt.geom.Area;
 
+/**
+ * Klassen produserer rektangel-objekt av type Shape
+ */
+
 public class MyRectangle extends Rectangle implements CollisionObjects {
 
     public MyRectangle(double x, double y, double width, double height){
         super(x, y, width, height);
     }
 
+    /**
+     * Metoden sjekker kollisjon med Bounds klassen
+     */
     public boolean detectCollision(Ball ball)
     {
 
@@ -18,6 +25,9 @@ public class MyRectangle extends Rectangle implements CollisionObjects {
         return player.intersects(thisRect);
     }
 
+    /**
+     *
+     */
     public void collisionEvent(Ball ball){
         ball.setVelocity(new BallVector(-ball.getVelocity().getX(), ball.getVelocity().getY()));
     }
