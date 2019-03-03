f39f372557456f2794e74a9130db7c06fe8f65d5
Jan Andreas <jan-anan@hotmail.com>
2019-03-03 12:32:20 +0100

    Flipper doc

--- src/Flipper.java
+++ src/Flipper.java
@@ -2,12 +2,23 @@ import javafx.geometry.Bounds;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * Klassen oppretter et ''Flipper'' Shape-objekt som spiller kan styre.
+ * Hensikten er å styre en flipper mot en kollisjon med ball
+ * så den holdes i spill
+ */
+
 public class Flipper extends Line implements CollisionObjects{
 
     public Flipper(double startX, double startY, double endX, double endY){
         super(startX, startY, endX, endY);
         setStrokeWidth(7);
     }
+
+
+    /**
+     * Metoden sjekker kollisjon med Bounds klassen
+     */
 
     public boolean detectCollision(Ball ball)
     {
@@ -16,6 +27,10 @@ public class Flipper extends Line implements CollisionObjects{
         Bounds thisRect = this.getBoundsInLocal();
         return player.intersects(thisRect);
     }
+
+    /**
+     * Metoden endrer Y-retningen
+     */
 
     public void collisionEvent(Ball ball){
         ball.setVelocity(new BallVector(ball.getVelocity().getX(), -ball.getVelocity().getY()));
